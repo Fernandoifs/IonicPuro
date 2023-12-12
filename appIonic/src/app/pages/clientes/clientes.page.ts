05402099ba39d4d867fa01cc67438c3a13a9da92
Fernandoifs <129119566+Fernandoifs@users.noreply.github.com>
2023-12-12 14:45:28 -0300

    12-12   botoes editar e apagar

--- appIonic/src/app/pages/clientes/clientes.page.ts
+++ appIonic/src/app/pages/clientes/clientes.page.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesPage implements OnInit {
 
-  constructor() { }
+  clientes = [
+    { id: 1, nome: 'Mark', telefone: 'Otto', pedido: '@mdo' },
+    { id: 2, nome: 'Jacob', telefone: 'Thornton', pedido: '@fat' },
+    { id: 3, nome: 'Larry', telefone: 'the Bird', pedido: '@twitter' },
+  ];
+
+  constructor() {}
 
   ngOnInit() {
   }
-
+  editarCliente(){}
+  apagarCliente(){}
 }
